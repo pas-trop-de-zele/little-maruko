2f19d5bd009bc546210489096f65484b8dfa91c0
Sam <huy.le09@yahoo.com>
2020-07-21 00:00:05 -0700

    Added POST and NEW route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require("express"),
+      mongoose = require("mongoose"),
+      bodyParser = require("body-parser")
 
 // Set up mongoose connection
 mongoose.connect("mongodb://localhost/LittleMaruko", { useNewUrlParser: true, useUnifiedTopology: true});
@@ -11,10 +12,13 @@ const PORT = 3000
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define TeaSchema
 const TeaSchema = new mongoose.Schema({
     name: String,
+    image: String,
+    description: String,
     price: Number
 });
 
@@ -24,15 +28,15 @@ const Tea = mongoose.model('Tea', TeaSchema);
 // ==================SEEDING DATA======================
 let seeds = [
     {
-        name: 'Original Thai Milk Tea',
+        name: 'Matcha Milk Tea',
+        image: "https://images.unsplash.com/photo-1561658286-ecb9fe9d8480?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        description: "Innovative Matcha",
         price: 4
     },
     {
         name: 'Green Thai Milk Tea',
-        price: 4
-    },
-    {
-        name: 'Golden Milk Tea',
+        image: "https://images.unsplash.com/photo-1482349274213-19ca6126f02f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1190&q=80",
+        description: "Unmatched Thai cuisine",
         price: 4
     },
 ]
@@ -48,6 +52,8 @@ Tea.deleteMany({}, (err, deleted_data) => {
 for (let i = 0; i < seeds.length; i++){
     Tea.create({
         name: seeds[i].name,
+        image: seeds[i].image,
+        description: seeds[i].description,
         price: seeds[i].price
     }, (err, new_data) => {
         if (err){
@@ -62,7 +68,7 @@ app.get('/', (req, res) => {
     res.render("index");
 })
 
-// Show all teas
+// INDEX route: show all teas
 app.get('/teas', (req, res) => {
     Tea.find({}, (err, teas) => {
         if (err) {
@@ -73,6 +79,25 @@ app.get('/teas', (req, res) => {
         }
     })
 })
+
+// NEW route: form to create new products
+app.get('/teas/new', (req, res) => {
+    res.render("new")
+})
+
+// CREATE ROUTE: create new route from form info
+app.post('/teas', (req, res) => {
+    Tea.create(req.body.tea, (err, new_tea) => {
+        if (err){
+            console.log(err)
+        } else {
+            console.log(new_tea)
+        }
+    })
+    res.redirect("/teas")
+})
+
+
 
 
 app.listen(PORT, () => {
