4f3405816b5bf52e9193aa902b2fd7a1f76d9e87
Sam <huy.le09@yahoo.com>
2020-07-21 07:42:27 -0700

    Added edit and update route

--- app.js
+++ app.js
@@ -48,16 +48,31 @@ app.get('/teas/new', (req, res) => {
     res.render("new")
 })
 
-// CREATE ROUTE: create new route from form info
+// CREATE route: create new route from form info
 app.post('/teas', (req, res) => {
-    Tea.create(req.body.tea, (err, new_tea) => {
+    Tea.create(req.body.tea, (err, newTea) => {
         if (err){
             console.log(err)
-        } else {
-            console.log(new_tea)
-        }
+        } 
     })
     res.redirect("/teas")
+})
+
+// EDIT route: edit current product
+app.get("/teas/:id/edit", (req, res) => {
+    Tea.findById(req.params.id, (err, foundTea) => {
+        res.render("edit", {tea: foundTea})
+    })
+})
+
+// UPDATE route: update current product
+app.put("/teas/:id", (req, res) => {
+    Tea.findByIdAndUpdate(req.params.id, req.body.updatedTea, (err, updated_tea) => {
+        if (err) {
+            console.log(err)
+        }
+        res.redirect("/teas")
+    })
 })
 
 // DESTROY route
