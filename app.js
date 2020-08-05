dfd8516182e792b9c5999627e2d54eb0070ea8a8
Sam <huy.le09@yahoo.com>
2020-08-04 23:57:18 -0700

    Restructured routes to a separate folder

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express"),
       methodOverride = require("method-override"),
       mongoose = require("mongoose"),
       bodyParser = require("body-parser"),
-      session = require("express-session"),
-      Tea = require("./models/teas")
+      teas = require("./routes/tea.js");
 
 // Set up mongoose connection
 mongoose.connect("mongodb://localhost/LittleMaruko", { useNewUrlParser: true, useUnifiedTopology: true});
@@ -13,69 +12,11 @@ const PORT = 3000
 
 // Misellaneous setup
 const app = express();
-app.use(session({secret: 'hs83kjgd83ks9201n5n22', resave: false, saveUninitialized: false}))
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"))
-
-// Landing page
-app.get('/', (req, res) => {
-    res.render("index");
-})
-
-// INDEX route: show all teas
-app.get('/teas', (req, res) => {
-    Tea.find({}, (err, teas) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.render("teas", {teas: teas})
-        }
-    })
-})
-
-// NEW route: form to create new products
-app.get('/teas/new', (req, res) => {
-    res.render("new")
-})
-
-// CREATE route: create new route from form info
-app.post('/teas', (req, res) => {
-    Tea.create(req.body.tea, (err, newTea) => {
-        if (err){
-            console.log(err)
-        } 
-    })
-    res.redirect("/teas")
-})
-
-// EDIT route: edit current product
-app.get("/teas/:id/edit", (req, res) => {
-    Tea.findById(req.params.id, (err, foundTea) => {
-        res.render("edit", {tea: foundTea})
-    })
-})
-
-// UPDATE route: update current product
-app.put("/teas/:id", (req, res) => {
-    Tea.findByIdAndUpdate(req.params.id, req.body.updatedTea, (err, updated_tea) => {
-        if (err) {
-            console.log(err)
-        }
-        res.redirect("/teas")
-    })
-})
-
-// DESTROY route
-app.delete("/teas/:id", (req, res) => {
-    Tea.findByIdAndDelete(req.params.id, (err) => {
-        if (err){
-            console.log(err)
-        }
-        res.redirect("/teas")
-    })
-})
+app.use("/", teas);
 
 // sign in route
 app.get("/user/signin", (req, res) => {
