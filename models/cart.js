c268d364bf8d0b37a4f91dbd771dad6e7bac199b
Sam <huy.le09@yahoo.com>
2020-08-08 00:20:20 -0700

    Added add function and generateProduct function to Car model

--- models/cart.js
+++ models/cart.js
@@ -1,11 +1,35 @@
-const mongoose = require("mongoose")
+class Cart {
+    constructor (cart){
+        // If cart object not exist yet will default to 0
+        this.items = cart.items || {};
+        // If totalQuantity not exist yet will default to 0
+        this.totalQuantity = cart.totalQuantity || 0;
+        // If totalPrice not exist yet will default to 0
+        this.totalPrice = cart.totalPrice || 0;
+    }
 
-// Define cart schema
-const CartItemSchema = new mongoose.Schema({
-    productId: String
-})
+    add(item, id) {
+        // Grab existing item in cart
+        let addItem = this.items[id];
+        // Check if item already exist
+        if (!addItem) {
+            addItem = this.items[id] = {item: item, qty: 0, price: 0}
+        }
+        this.items[id].qty++;
+        this.totalPrice += this.items[id].item.price;
+        this.items[id].price = this.items[id].item.price * this.items[id].qty;
+        this.totalQuantity++;
+        
+    }
 
-// Compile CartItemSchema into model
-const CartItem = mongoose.model('CartItem', CartItemSchema)
+    // Right now this.items is an object, we need to out put this to array
+    generateProducts() {
+        let products = [];
+        for (let id in this.items) {
+            products.push(this.items[id]);
+        }
+        return products;
+    }
+}
 
-module.exports = CartItem
+module.exports = Cart;
