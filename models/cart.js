11b90b559984df528edc7a7af1f39b76b7f6cf7e
Sam <huy.le09@yahoo.com>
2020-08-10 23:53:02 -0700

    Added methods to remove item and update item quantity

--- models/cart.js
+++ models/cart.js
@@ -22,6 +22,27 @@ class Cart {
         
     }
 
+    remove(id) {
+        this.totalQuantity -= this.items[id].qty;
+        this.totalPrice -= this.items[id].price;
+        delete this.items[id];
+    }
+
+    updateQuantity(id, newQuantity) {
+        let selectItem = this.items[id];
+        let changeInQuantity = selectItem.qty - newQuantity;
+        // Update the item quantity
+        selectItem.qty = newQuantity;
+        
+        let newPrice = newQuantity * selectItem.item.price;
+        let changeInPrice = selectItem.price - newPrice;
+        // Update the item price
+        selectItem.price -= changeInPrice;
+        
+        this.totalQuantity -= changeInQuantity;
+        this.totalPrice -= changeInPrice;
+    }
+
     // Right now this.items is an object, we need to out put this to array
     generateProducts() {
         let products = [];
