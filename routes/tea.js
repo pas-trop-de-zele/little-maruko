a77924c98184882a15b114fd4b5f062bc9498f85
Sam <huy.le09@yahoo.com>
2020-08-11 06:28:52 -0700

    Restructure cart routes to a seprate file

--- routes/tea.js
+++ routes/tea.js
@@ -61,49 +61,4 @@ router.delete("/teas/:id", middleware.isLoggedin, (req, res) => {
     })
 })
 
-
-// Add to cart route
-router.get("/add-to-cart/:id", (req, res) => {
-    // Retrieve product id
-    let teaId = req.params.id;
-    let cart = new Cart(req.session.cart ? req.session.cart : {});
-
-    // Find item in database through id
-    Tea.findById(teaId, (err, foundTea) => {
-        if (err){
-            console.log(err);
-        } else{
-            // Add item object to cart
-            cart.add(foundTea, teaId);
-            // Save cart to seesion
-            req.session.cart = cart;
-            res.redirect("/teas");
-        }
-    })
-})
-
-// View cart
-router.get("/show-cart", (req, res) => {
-    // Use existing cart in session if there is one otherwise use blank one
-    let cart = new Cart(req.session.cart ? req.session.cart : {});
-    let teasInCart = cart.generateProducts();
-    res.render("cart/cart", {teasInCart : teasInCart, cart : cart});
-})
-
-// remove item off cart
-router.get("/cart/:id", (req, res) => {
-    let cart = new Cart(req.session.cart);
-    cart.remove(req.params.id);
-    req.session.cart = cart;
-    res.redirect("/show-cart");
-})
-
-// update item quantity in cart
-router.post("/cart/:id/updatequantity", (req, res) => {
-    let cart = new Cart(req.session.cart);
-    cart.updateQuantity(req.params.id, req.body.newQuantity);
-    req.session.cart = cart;
-    res.redirect("/show-cart");
-})
-
 module.exports = router;
