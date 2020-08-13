b44715fa9fe9c01ae0659ee8d70b634e7a644858
Sam <huy.le09@yahoo.com>
2020-08-13 07:37:12 -0700

    Overhaul done, no more sending mailthrough nodemailer, create a dashboard to manage order instead

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,51 +1,25 @@
 const router = require("express").Router(),
-      nodemailer = require("nodemailer"),
-      smtpCredentials = require("../credentials/smtp"),
-      Cart = require("../models/cart");
+      Order = require("../models/order"),
+      Cart = require("../models/cart"),
+      bodyParser = require("body-parser");
 
-// transport object
-const mailman = nodemailer.createTransport({
-    host: smtpCredentials.host,
-    port: smtpCredentials.port,
-    auth: {
-        user: smtpCredentials.user,
-        pass: smtpCredentials.pass
-    }
-})
+router.use(bodyParser.urlencoded({extended: true}));
 
-router.post('/checkout', (req, res) => {
+router.post("/checkout", (req, res) => {
     let cart = new Cart(req.session.cart);
-    let items = cart.generateProducts();
-    let order = "";
-    // Add pick up info
-    order += `
-        <h3>Customer info</h3>
-        Name: ${req.body.name} <br>
-        Phone: ${req.body.phone} <br>
-        Pickup date: ${req.body.date} <br>
-        Pickup time: ${req.body.time} <br>
-
-        <h3>Order info</h3> </br>
-    `
-    // Add items
-    items.forEach((item) => {
-        order += `${item.item.name} - Quantity: ${item.qty} - Subtotal: $${item.price} <br>`;
-    })
-    order += `Total: $${cart.totalPrice}`
-    // clear cart
-    cart.clearAll();
-    req.session.cart = cart;
-    const mail = {
-        from: smtpCredentials.user,
-        to: smtpCredentials.user,
-        subject: `NEW ORDER from  ${req.body.name}`,
-        html: order
-    }
-    mailman.sendMail(mail, (err, info) => {
+    Order.create({
+        cart: cart.generateProducts(),
+        name: req.body.name,
+        phone: req.body.phone,
+        date: req.body.date,
+        time: req.body.time,
+        total: cart.totalPrice
+    }, (err, newOrder) => {
         if (err) {
             return console.log(err);
         }
-        console.log(`Send order to ${info.messageId}`)
+        cart.clearAll();
+        req.session.cart = cart;
         res.render("cart/checkoutDone");
     })
 })
