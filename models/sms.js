42c5b5a720e283bf09e36b230dd8a8e8c104f2e5
Sam <huy.le09@yahoo.com>
2020-08-14 08:29:36 -0700

    add callback to methods after sending msgs

--- models/sms.js
+++ models/sms.js
@@ -7,24 +7,32 @@ class Sms {
         this.client = require("twilio")(this.accountSid, this.authToken);
     }
 
-    messageConfirmReceived(name, number) {
+    messageConfirmReceived(name, number, callback) {
         this.client.messages
             .create({
                 body: `Hello ${name}, your order was received, we will notify you when it is ready for pick up`,
                 from: TwilioCredentials.phone,
+                statusCallback: "http://48e196ff88ee.ngrok.io/dashboard/status",
                 to: `+1${number}`
             })
-            .then(message => {console.log(`Notify order received for ${name} - ${number}`)});
+            .then(message => {
+                console.log(`Notify order received for ${name} - ${number}`);
+                callback();
+            });
     }
 
-    messasgeReadyToPickUp(name, number) {
+    messasgeReadyToPickUp(name, number, callback) {
         this.client.messages
             .create({
                 body: `Hello ${name}, your order is ready for pick up`,
                 from: TwilioCredentials.phone,
+                statusCallback: "http://48e196ff88ee.ngrok.io/dashboard/status",
                 to: `+1${number}`
             })
-            .then(message => {console.log(`Notify order ready for pick up for ${name} - ${number}`)});
+            .then(message => {
+                console.log(`Notify order ready for pick up for ${name} - ${number}`);
+                callback();
+            });
     }
 }
 
