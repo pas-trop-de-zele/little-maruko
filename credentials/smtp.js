3030a87da763c797d7da2ddda70e643f528eb9f8
Sam <huy.le09@yahoo.com>
2020-10-19 00:25:19 -0700

    Removed credentials file from repo

--- credentials/smtp.js
+++
@@ -1,8 +0,0 @@
-const smtp = {
-    user: "little-maruko@outlook.com",
-    pass: "qcf4me!!",
-    host: "smtp-mail.outlook.com",
-    port: 25
-}
-
-module.exports = smtp;
--- credentials/twilio.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    accountSid: "AC5d6ecd4c05f31144a53062cf1c7ef793",
-    authToken: "5e88c4278a1afe4b0b99e428e0f61184",
-    phone: "+17147930622"
-}
